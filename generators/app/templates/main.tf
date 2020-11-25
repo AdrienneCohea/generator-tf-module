55f69fdce288a0d71bb298a2177259a6aadefbdb
Adrienne Cohea <34219237+AdrienneCohea@users.noreply.github.com>
2020-11-24 23:16:59 -0800

    Support Terraform 0.14 RC1 (#1)
    Use Terraform 0.14 RC1 and create an example compute instance

---
+++ generators/app/templates/main.tf
@@ -0,0 +1,26 @@
+resource "google_compute_instance" "example" {
+  name         = "example-yeoman-generator"
+  machine_type = "e2-medium"
+  zone         = "us-west1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+
+  metadata = {
+    "user-data" = file("${path.module}/templates/cloud-config.yaml")
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
